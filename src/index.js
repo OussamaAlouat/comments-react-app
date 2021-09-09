69ee2daed462af74f79b8a6ab77691dfb8b363cf
oalouat <oussama.alouat@accenture.com>
2021-09-09 10:40:19 +0200

    Added two comments more

--- src/index.js
+++ src/index.js
@@ -16,7 +16,55 @@ const App = () => {
             </a>
 
             <div className="content">
-              <a className="author">Stevie Feliciano</a>
+              <a className="author">{faker.name.firstName()}</a>
+              <div className="metadata">
+                <div className="date">2 days ago</div>
+                <div className="rating">
+                  <i className="star icon"></i>
+                  5 Faves
+                </div>
+              </div>
+              <div className="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="ui comments">
+          <div className="comment">
+
+            <a className="avatar">
+              <img src={faker.image.avatar()}/>
+            </a>
+
+            <div className="content">
+              <a className="author">{faker.name.firstName()}</a>
+              <div className="metadata">
+                <div className="date">2 days ago</div>
+                <div className="rating">
+                  <i className="star icon"></i>
+                  5 Faves
+                </div>
+              </div>
+              <div className="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="ui comments">
+          <div className="comment">
+
+            <a className="avatar">
+              <img src={faker.image.avatar()}/>
+            </a>
+
+            <div className="content">
+              <a className="author">{faker.name.firstName()}</a>
               <div className="metadata">
                 <div className="date">2 days ago</div>
                 <div className="rating">
