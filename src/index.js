643de8061e3f2eb9071e33e0c8a305d9fd1d3076
oalouat <oussama.alouat@accenture.com>
2021-09-09 10:33:22 +0200

    Add commentaries section

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ const App = () => {
   return (
     <div className="container">
       <h1>Wellcome to Comment APP</h1>
+      <div className="comments-section">
+        <div className="ui comments">
+          <div className="comment">
+
+            <a className="avatar">
+              <img src="/images/avatar/small/stevie.jpg"/>
+            </a>
+
+            <div className="content">
+              <a className="author">Stevie Feliciano</a>
+              <div className="metadata">
+                <div className="date">2 days ago</div>
+                <div className="rating">
+                  <i className="star icon"></i>
+                  5 Faves
+                </div>
+              </div>
+              <div className="text">
+                Hey guys, I hope this example comment is helping you read this documentation.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
