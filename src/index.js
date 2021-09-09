65584d92cc89b011c469a19ba71dd6ec6d0cea8a
oalouat <oussama.alouat@accenture.com>
2021-09-09 11:05:49 +0200

    Used function isntead of repeat three times the html section

--- src/index.js
+++ src/index.js
@@ -4,81 +4,45 @@ import './app.css'
 import faker from 'faker'
 faker.locale = "es";
 
+function getCommentaries () {
+  const commentaries = [];
+  const randomNumber = Math.random() * (10 - 3) + 3;
+  for (let i = 0; i < randomNumber; i++) {
+    commentaries.push(
+      <div className="ui comments" >
+        <div className="comment">
+          <a className="avatar">
+            <img src={ faker.image.avatar() } alt="avatar"/>
+          </a>
+
+          <div className="content">
+            <h3 className="author">{ faker.name.firstName() }</h3>
+            <div className="metadata">
+              <div className="date">2 days ago</div>
+            <div className="rating">
+              <i className="star icon"></i>
+              5 Faves
+            </div>
+          </div>
+            <div className="text">
+              { faker.lorem.sentence() }
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return commentaries;
+}
+
+
 const App = () => {
   return (
     <div className="container">
       <h1>Wellcome to Comment APP</h1>
       <div className="comments-section">
-        <div className="ui comments" >
-          <div className="comment">
-
-            <a className="avatar">
-              <img src={faker.image.avatar()} alt="avatar"/>
-            </a>
-
-            <div className="content">
-              <h3 className="author">{faker.name.firstName()}</h3>
-              <div className="metadata">
-                <div className="date">2 days ago</div>
-                <div className="rating">
-                  <i className="star icon"></i>
-                  5 Faves
-                </div>
-              </div>
-              <div className="text">
-                {faker.lorem.sentence()}
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="ui comments">
-          <div className="comment">
-
-            <a className="avatar">
-              <img src={faker.image.avatar()} alt="avatar"/>
-            </a>
-
-            <div className="content">
-              <h3 className="author">{faker.name.firstName()}</h3>
-              <div className="metadata">
-                <div className="date">2 days ago</div>
-                <div className="rating">
-                  <i className="star icon"></i>
-                  5 Faves
-                </div>
-              </div>
-              <div className="text">
-                { faker.lorem.sentence() }
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="ui comments">
-          <div className="comment">
-
-            <a className="avatar">
-              <img src={faker.image.avatar()} alt="avatar"/>
-            </a>
-
-            <div className="content">
-              <h3 className="author">{faker.name.firstName()}</h3>
-              <div className="metadata">
-                <div className="date">2 days ago</div>
-                <div className="rating">
-                  <i className="star icon"></i>
-                  5 Faves
-                </div>
-              </div>
-              <div className="text">
-                { faker.lorem.sentence() }
-              </div>
-            </div>
-          </div>
-        </div>
+        { getCommentaries() }
       </div>
     </div>
   )
